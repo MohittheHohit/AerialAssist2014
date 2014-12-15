fca881e1a22c8e9e086780687f00c5ffe1db03ce
MohittheHohit <mohit19k9@gmail.com>
2014-12-15 15:53:41 -0500

    ZigZag Assignment
    Probably mAKEs  A ZIg zag

--- Commands/AutonomousModeOneBall.cpp
+++ Commands/AutonomousModeOneBall.cpp
@@ -14,14 +14,17 @@
 
 AutonomousModeOneBall::AutonomousModeOneBall()
 {
-  AddSequential(new AutonomousLowGearCommand());
-  AddSequential(new LoadCatapultGroup());
-  AddSequential(new OperatorLowCommand());
-  AddSequential(new AutonomousPickupCommand(2.0));
-  AddSequential(new OperatorPickup2Command());
-  AddSequential(new OperatorHighCommand());
   AddSequential(new AutonomousDriveCommand(1.0f, TURN_CORRECTION, 2.2));
-  AddParallel(new OperatorPickup2Command(0.5));
-  AddSequential(new AutonomousCoastCommand(1.0f, TURN_CORRECTION, 0.0f, TURN_CORRECTION, 0.5));
-  AddSequential(new DriveLaunchReleaseCommand());
+  AddSequential(new AutonomousDriveCommand(0f, TURN_CORRECTION, 0));
+  Wait (2.0f);
+  AddSequential(new AutonomousDriveCommand(1.0f, 1.0, 2.2));
+  AddSequential(new AutonomousDriveCommand(0f, 1.0, 0));
+  AddSequential(new AutonomousDriveCommand(0f, TURN_CORRECTION, 0));
+  Wait (2.0f);
+  AddSequential(new AutonomousDriveCommand(1.0f, -1.0, 2.2));
+  AddSequential(new AutonomousDriveCommand(0.0f, TURN_CORRECTION, 0));
+  Wait (2.0f);
+  AddSequential(new AutonomousDriveCommand(1.0f, TURN_CORRECTION, 2.2));
+  AddSequential(new AutonomousDriveCommand(0.0f, TURN_CORRECTION, 0));
+  Wait (2.0f);
 }
